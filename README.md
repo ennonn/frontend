1c8a603b89420bfaba8825a32b2da70b47927d1b
Ennonn <aljonfernando09@gmail.com>
2023-12-17 13:37:45 +0800

    Updated

--- README.md
+++ README.md
@@ -1,9 +1,53 @@
 # frontend
 
-## Project setup
+
+### Modified Readme
+
+### Install VUE CLI globally
 ```
-npm install
+npm install -g @vue/cli
 ```
+
+### Create New Vue project
+```
+vue create my-vue-project
+```
+
+### Copy and Replace these following:
+```public
+```
+```src
+```
+
+### Install these:
+
+
+### VUEX
+```
+npm install vuex
+```
+
+```
+npm install vuex-persistedstate
+```
+
+### JQUERY
+```
+npm install jquery
+```
+
+###  axios, v-pagination-3, vue3-select2-component, and vue-router
+```
+npm install axios v-pagination-3 vue3-select2-component vue-router
+```
+
+### Add this rule to your package
+```
+"rules": {
+      "vue/multi-word-component-names": "off", //add this line
+    }
+```    
+
 
 ### Compiles and hot-reloads for development
 ```
@@ -22,3 +66,4 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="get_check_auth_status">
+      <dashboard></dashboard>
+    </div>
+
+    <div v-else>
+      <router-view />
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import dashboard from "./views/backend/dashboard.vue";
+import { mapGetters, mapMutations } from "vuex";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  components: { dashboard },
+  data: function () {
+    return {};
+  },
+  created: function () {
+    this.chech_auth_role();
+    // console.log(this.get_check_auth_status);
+    this.check_auth_status();
+  },
+  updated: function () {
+    console.log(this.get_check_auth_status);
+    console.log(this.get_auth_role_name);
+  },
+  watch: {
+    get_check_auth_status: {
+      handler: function () {
+        this.check_auth_status();
+      },
+    },
+  },
+  methods: {
+    // ...mapActions(['']),
+    ...mapMutations(["set_logout", "set_auth_token"]),
+    chech_auth_role: function () {
+      if (this.get_check_auth_status) {
+        if (this.get_auth_role_name == "admin") {
+          this.$router.replace({ name: "admin" });
+        } else if (this.get_auth_role_name == "student") {
+          this.$router.replace({ name: "student" });
+        } else if (this.get_auth_role_name == "management") {
+          this.$router.replace({ name: "management" });
+        } else {
+          this.set_logout();
+          this.$router.replace({ name: "login" });
+        }
+      } else {
+        // this.$router.replace({ name: "login" });
+        this.$router.replace({ name: "homePage" });
+      }
+    },
+    check_auth_status: function () {
+      console.log("changed status");
+      this.chech_auth_role();
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      if (this.get_check_auth_status == true) {
+        console.log('set token');
+        window.axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${this.get_auth_token}`;
+      } else {
+        this.set_auth_token({ token: null });
+        delete window.axios.defaults.headers.common.Authorization;
+      }
+    },
+  },
+  computed: {
+    ...mapGetters([
+      "get_check_auth_status",
+      "get_auth_role_name",
+      "get_auth_token",
+    ]),
+  },
+};
+</script>
